f6396bc09dfd18d64b97cfb88d083a02ae6beede
Jeffrey Valeroso <jfvaleroso.smart@gmail.com>
2016-01-30 01:35:38 +0800

    updated name

--- angular-digit-directive.js
+++ angular-digit-directive.js
@@ -1,6 +1,6 @@
 (function (ng) {
   'use strict'
-  ng.module('angularOnlyNumber", [])
+  ng.module('angular.only.number', [])
     .directive('ng-only-number', function () {
       // this directive allowes only numbers with decimal point.
       // You can specify the max decimal points using data-max-decimal-points in the dom element.
